cc498c9e1473174d4561606fbc0a50983a674433
michael <huangmichael150@gmail.com>
2022-03-15 10:54:14 -0500

    finish core of coffeerun

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,8 @@
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+
+    let myTruck = new Truck('12345', new DataStore());
+
+    window.myTruck = myTruck;
 })(window);
