e24d81369c1b04db11929d2ec381b16426900523
mhuang03 <huangmichael150@gmail.com>
2022-03-17 10:18:13 -0500

    coffeerun done

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,19 @@
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
     const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    const SERVER_URL = 'https://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
 
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+    let RemoteDataStore = App.RemoteDataStore;
     let FormHandler = App.FormHandler;
     let CheckList = App.CheckList;
     let Validation = App.Validation;
 
-    let myTruck = new Truck('12345', new DataStore());
+    let remoteDS = new RemoteDataStore(SERVER_URL);
+
+    let myTruck = new Truck('12345', remoteDS);
     window.myTruck = myTruck;
 
     let formHandler = new FormHandler(FORM_SELECTOR);
